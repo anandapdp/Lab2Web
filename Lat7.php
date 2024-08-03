d2a9b92fe26b919614343e7e804b47a7ff7abc0d
anandapdp <168748726+anandapdp@users.noreply.github.com>
2024-08-03 10:56:03 +0700

    Delete Lat7.php

--- Lat7.php
+++
@@ -1,9 +0,0 @@
-<h2>Perulangan While</h2>
-<?php
-        echo "Perulangan 1 sampai 10 <br />";
-        $i=1;
-    while ($i<=10) {
-        echo "Perulangan ke: " . $i . '<br />';
-        $i++;
-    }
-?>
