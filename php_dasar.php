74a390c57382339c33836afd2bf47fcde6c19eb5
anandapdp <168748726+anandapdp@users.noreply.github.com>
2024-08-03 10:52:22 +0700

    lab2_php_dasar

---
+++ php_dasar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+        echo "Hello World";
+    ?>
+</body>
+</html>
+
+<!-- Variabel PHP -->
+<h2>Menggunakan Variabel</h2>
+    <?php
+        $nim = "312210435";
+        $nama = 'Anannda Putri Dwi Permatasari';
+        echo "NIM : " . $nim . "<br>";
+        echo "Nama : $nama";
+    ?>
