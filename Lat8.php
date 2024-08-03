dde9be0f1a4ba235c38bcd97940cefa043ef2862
anandapdp <168748726+anandapdp@users.noreply.github.com>
2024-08-03 10:56:19 +0700

    Delete Lat8.php

--- Lat8.php
+++
@@ -1,9 +0,0 @@
-<h2>Perulangan Dowhile</h2>
-<?php
-        echo "Perulangan 1 sampai 10 <br />";
-        $i=1;
-    do {
-        echo "Perulangan ke: " . $i . '<br />';
-        $i++;
-        } while ($i<=10);
-?>
